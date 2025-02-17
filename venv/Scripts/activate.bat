c531d9ac9a5d3f05b0f5da900584af664b25d241
Russell Sebastian <russell@thegrapevineescape.com>
2025-02-16 18:31:27 -0600

    Admin Calendar Dashboard css

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\My Drive\The Grapevine Escape\MysteriesInMotion\venv
+set VIRTUAL_ENV=G:\My Drive\The Grapevine Escape\MysteriesInMotion\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
