84782ef1f62b5cc50652924e40bb1fc51207c39f
Russell Sebastian <russell@thegrapevineescape.com>
2025-02-20 19:58:21 -0600

    Performer Management - Admin 022025

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -4,7 +4,6 @@ from flask_login import login_required, current_user
 from app.models import RoleApplication, User, Show, Role, ShowDate, Rehearsal
 from app.extensions import db
 from app.admin.forms import ShowForm, RoleForm
-from app.admin import admin_bp 
 import logging
 from datetime import datetime
 
